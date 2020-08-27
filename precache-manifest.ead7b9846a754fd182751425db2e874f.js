893f469968bf31e96e90c1a43a50630da1ac793c
PatrickLensen337 <patrick@patricklensen.nl>
2020-08-27 23:32:45 +0200

    Updates

---
+++ precache-manifest.ead7b9846a754fd182751425db2e874f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa026d3f24b527b933e275c030d2120f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8e3dc63a92f4938dd2f4",
+    "url": "/monsters-rolodex/static/css/main.ae5d8534.chunk.css"
+  },
+  {
+    "revision": "f9dcfd166a0dbd3a0d85",
+    "url": "/monsters-rolodex/static/js/2.e2e0115a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e2e0115a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3dc63a92f4938dd2f4",
+    "url": "/monsters-rolodex/static/js/main.e65637d6.chunk.js"
+  },
+  {
+    "revision": "7c7a2dec0da0b94e3a2d",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfbc620.js"
+  }
+]);
